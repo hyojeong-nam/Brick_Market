b50abffea67945dd0005816c96ef9bcc447f5840
김진혁 <kjh153426@gmail.com>
2023-01-27 10:20:21 +0900

    DBCP 설정

--- brick_market/src/main/java/com/team4/db/Team4DB.java
+++ brick_market/src/main/java/com/team4/db/Team4DB.java
@@ -1,5 +1,25 @@
 package com.team4.db;
 
-public class Team4DB {
+import java.sql.Connection;
+import javax.sql.*;
+import javax.naming.*;
 
+
+public class Team4DB {
+	static DataSource ds;
+	
+	static {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context)initContext.lookup("java:/comp/env");
+			ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConn() throws Exception {
+		return ds.getConnection();
+	}
+	
 }
