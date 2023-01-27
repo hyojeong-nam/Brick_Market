7d382f43c5998386e3465c53d323182a6e9461a6
김진혁 <kjh153426@gmail.com>
2023-01-27 11:09:55 +0900

    MemberDAO / MemberDTO 설정 회원가입 / 로그인 / 조회 기능

--- brick_market/src/main/java/com/team4/member/MemberDAO.java
+++ brick_market/src/main/java/com/team4/member/MemberDAO.java
@@ -1,5 +1,103 @@
 package com.team4.member;
 
-public class MemberDAO {
+import java.sql.*;
+import java.util.*;
 
+public class MemberDAO {
+	
+	Connection conn;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	/**회원 가입*/
+	public int joinMember(MemberDTO dto) {
+		try {
+			conn = com.team4.db.Team4DB.getConn();
+			String sql = "insert into member_table values (member_table_idx.nextval,?,?,?,?,?,sysdate,'img',0)";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, dto.getMember_id());
+			ps.setString(2, dto.getMember_pwd());
+			ps.setString(3, dto.getMember_name());
+			ps.setString(4, dto.getMember_nick());
+			ps.setString(5, dto.getMember_email());
+			int count = ps.executeUpdate();
+			return count;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		} finally {
+			try {
+				if(ps!=null)ps.close();
+				if(conn!=null)conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	/**로그인*/
+	public boolean checkLogin(String id, String pwd) {
+		try {
+			conn = com.team4.db.Team4DB.getConn();
+			String sql = "select * from member_table where id = ?, pwd = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.setString(2, pwd);
+			rs = ps.executeQuery();
+			
+			boolean result = rs.next();
+			return result;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null)ps.close();
+				if(conn!=null)conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	/**idx로 검색*/
+	public MemberDTO searchIdx(int idx) {
+		try {
+			conn = com.team4.db.Team4DB.getConn();
+			String sql = "select * from idx = ?";
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				
+				String member_id = rs.getString("member_id");
+				String member_pwd = rs.getString("member_pwd");
+				String member_name = rs.getString("member_name");
+				String member_nick = rs.getString("member_nick");
+				String memeber_email = rs.getString("member_email");
+				java.sql.Date member_date = rs.getDate("member_date");
+				String member_img = rs.getString("member_img");
+				int memeber_class = rs.getInt("member_class");
+				
+				MemberDTO dto = new MemberDTO(idx, member_id, member_pwd, member_name, member_nick, memeber_email, member_date, member_img, memeber_class);
+				return dto;
+			}else {
+				return null;
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
 }
--- brick_market/src/main/java/com/team4/member/MemberDTO.java
+++ brick_market/src/main/java/com/team4/member/MemberDTO.java
@@ -1,5 +1,87 @@
 package com.team4.member;
 
-public class MemberDTO {
+import java.sql.Date;
 
+public class MemberDTO {
+	private int member_idx;
+	private String member_id;
+	private String member_pwd;
+	private String member_name;
+	private String member_nick;
+	private String member_email;
+	private Date member_date;
+	private String member_img;
+	private int member_class;
+	public MemberDTO() {
+		super();
+	}
+	public MemberDTO(int member_idx, String member_id, String member_pwd, String member_name, String member_nick,
+			String member_email, Date member_date, String member_img, int member_class) {
+		super();
+		this.member_idx = member_idx;
+		this.member_id = member_id;
+		this.member_pwd = member_pwd;
+		this.member_name = member_name;
+		this.member_nick = member_nick;
+		this.member_email = member_email;
+		this.member_date = member_date;
+		this.member_img = member_img;
+		this.member_class = member_class;
+	}
+	public int getMember_idx() {
+		return member_idx;
+	}
+	public void setMember_idx(int member_idx) {
+		this.member_idx = member_idx;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pwd() {
+		return member_pwd;
+	}
+	public void setMember_pwd(String member_pwd) {
+		this.member_pwd = member_pwd;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_nick() {
+		return member_nick;
+	}
+	public void setMember_nick(String member_nick) {
+		this.member_nick = member_nick;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public Date getMember_date() {
+		return member_date;
+	}
+	public void setMember_date(Date member_date) {
+		this.member_date = member_date;
+	}
+	public String getMember_img() {
+		return member_img;
+	}
+	public void setMember_img(String member_img) {
+		this.member_img = member_img;
+	}
+	public int getMember_class() {
+		return member_class;
+	}
+	public void setMember_class(int member_class) {
+		this.member_class = member_class;
+	}
+	
+	
 }
