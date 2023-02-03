7e4df4dd7c303d49973dece0442af1c7ce59ef33
NamHyoJeong <rittoya@daum.net>
2023-02-03 15:25:39 +0900

    reportDAO, DTO 생성 / footer에 신고하기 삭제 (게시글 안에서 신고하기로 변경 예정) / report.jsp 외견만  /  reportList.jsp 삭제 (게시글에서 경고 띄우기로 변경 예정)

--- brick_market/src/main/java/com/team4/report/ReportDTO.java
+++ brick_market/src/main/java/com/team4/report/ReportDTO.java
@@ -1,5 +1,72 @@
 package com.team4.report;
 
-public class ReportDTO {
+import java.sql.Date;
 
+public class ReportDTO {
+	
+	private int report_idx;
+	private int report_get_user_idx;
+	private int report_send_user_idx;
+	private Date report_date;
+	private String report_content;
+	
+	public ReportDTO() {
+		super();
+	}
+
+	public ReportDTO(int report_idx, int report_get_user_idx, int report_send_user_idx, Date report_date,
+			String report_content) {
+		super();
+		this.report_idx = report_idx;
+		this.report_get_user_idx = report_get_user_idx;
+		this.report_send_user_idx = report_send_user_idx;
+		this.report_date = report_date;
+		this.report_content = report_content;
+	}
+
+	public int getReport_idx() {
+		return report_idx;
+	}
+
+	public void setReport_idx(int report_idx) {
+		this.report_idx = report_idx;
+	}
+
+	public int getReport_get_user_idx() {
+		return report_get_user_idx;
+	}
+
+	public void setReport_get_user_idx(int report_get_user_idx) {
+		this.report_get_user_idx = report_get_user_idx;
+	}
+
+	public int getReport_send_user_idx() {
+		return report_send_user_idx;
+	}
+
+	public void setReport_send_user_idx(int report_send_user_idx) {
+		this.report_send_user_idx = report_send_user_idx;
+	}
+
+	public Date getReport_date() {
+		return report_date;
+	}
+
+	public void setReport_date(Date report_date) {
+		this.report_date = report_date;
+	}
+
+	public String getReport_content() {
+		return report_content;
+	}
+
+	public void setReport_content(String report_content) {
+		this.report_content = report_content;
+	}
+
+	
+	
 }
+
+
+
---
+++ brick_market/src/main/webapp/report/report_ok.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+request.setCharacterEncoding("utf-8");
+%>
+<jsp:useBean id="rdto" class="com.team4.report.ReportDTO"></jsp:useBean>
+<jsp:setProperty property="*" name="mdto"/>
+<jsp:useBean id="rdao" class="com.team4.report.ReportDAO"></jsp:useBean>
+<%
+%>
